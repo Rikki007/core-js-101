392fb610021b1bb67819d07e1ce41d6022cefc23
Kirill Oreshkevich <Oreshaodnoglazy@gmail.com>
2023-09-27 20:40:19 +0300

    feat: add some solutions

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -50,15 +50,8 @@ function getCircleCircumference(radius) {
  *  -3, 3  => 0
  */
 function getAverage(value1, value2) {
-  let res = 0;
-  if (value1 + value2 < 0) {
-    res = -1;
-  } else if (value1 + value2 === 0) {
-    res = 0;
-  } else {
-    res = (value1 + value2) / 2;
-  }
-  return res;
+  const a = value1 / 2 + value2 / 2;
+  return a;
 }
 
 /**
@@ -76,8 +69,9 @@ function getAverage(value1, value2) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  const a = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+  return a;
 }
 
 /**
@@ -92,8 +86,9 @@ function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot(a, b) {
+  const x = -b / a;
+  return x;
 }
 
 /**
@@ -130,8 +125,9 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  const ret = value.toString();
+  return +ret[ret.length - 1];
 }
 
 /**
@@ -145,8 +141,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return +value;
 }
 
 /**
@@ -162,8 +158,9 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  const d = Math.sqrt(a ** 2 + b ** 2 + c ** 2);
+  return d;
 }
 
 /**
@@ -183,8 +180,9 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const a = Math.round(num / 10 ** pow) * 10 ** pow;
+  return a;
 }
 
 /**
@@ -204,8 +202,14 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  let calc = 0;
+  for (let i = 1; i <= n; i += 1) {
+    if (n % i === 0) {
+      calc += 1;
+    }
+  }
+  return calc <= 2;
 }
 
 /**
@@ -223,8 +227,8 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  return +value || def;
 }
 
 module.exports = {
