7199018ca69f47fceea0568884c069104bab2390
Kirill Oreshkevich <Oreshaodnoglazy@gmail.com>
2023-10-16 21:00:13 +0300

    feat: solve some tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -240,8 +240,12 @@ function toArrayOfSquares(arr) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
+function getMovingSum(arr) {
+  let calc = 0;
+  return arr.map((item) => {
+    calc += item;
+    return calc;
+  });
 }
 
 /**
@@ -255,8 +259,8 @@ function getMovingSum(/* arr */) {
  * [ 'a', 'b', 'c' , null ]  => [ "b", null ]
  * [ "a" ] => []
  */
-function getSecondItems(/* arr */) {
-  throw new Error('Not implemented');
+function getSecondItems(arr) {
+  return arr.filter((item, index) => index % 2 !== 0);
 }
 
 /**
