aaeaa96a4c09b9cccdccaebd100bf9291abfadbb
Kirill Oreshkevich <Oreshaodnoglazy@gmail.com>
2023-10-17 19:11:25 +0300

    feat: solve some task

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -277,8 +277,14 @@ function getSecondItems(arr) {
  *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  const array = [];
+  arr.forEach((item, ind) => {
+    for (let i = 0; i < ind + 1; i += 1) {
+      array.push(item);
+    }
+  });
+  return array;
 }
 
 /**
